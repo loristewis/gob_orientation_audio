729be8ff76fa71fcfd901ce59744ac59f88dc70c
Loris Birkemeyer <loris.birkemeyer@edu.gobelins.fr>
2021-10-22 11:02:05 +0200

    feat: trads

--- front/src/config.js
+++ front/src/config.js
@@ -2,7 +2,7 @@ export default {
   app: document.getElementById('app'),
   i18n: {
     welcomeScreen: 'click or tap anywhere',
-    pause: 'click here to pause',
-    play: 'click here to play'
+    pause: 'click here to pause sound',
+    play: 'click here to play sound'
   }
 }
