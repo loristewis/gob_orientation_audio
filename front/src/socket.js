e38f93ca76fe08243c60579fc9fb1f9deb5e44b6
Loris Birkemeyer <loris.birkemeyer@edu.gobelins.fr>
2021-10-22 10:14:56 +0200

    feat: hardcode url

--- front/src/socket.js
+++ front/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 import config from './config'
 
-const socket = io(process.env.BACKEND_HEROKU_URL)
+const socket = io('https://loristewis-getmovements.herokuapp.com/')
 
 export default socket
